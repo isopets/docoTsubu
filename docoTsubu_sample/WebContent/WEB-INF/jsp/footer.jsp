292ca627c1ecd5a7f1cd75851050e21b6b0ed770
磯貝　雄人 <47950027+isopets@users.noreply.github.com>
2022-01-20 20:57:39 +0900

    Delete docoTsubu_sample directory

--- docoTsubu_sample/WebContent/WEB-INF/jsp/footer.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<p>Copyright どこつぶ制作委員会　All Rights Reserved.</p>
--- docoTsubu_sample/src/model/PostMutterLogic.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.List;
-
-public class PostMutterLogic {
-  public void execute(Mutter mutter, List<Mutter> mutterList) {
-    mutterList.add(0, mutter); // 先頭に追加 解説①
-  }
-}
